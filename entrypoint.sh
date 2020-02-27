f3a33fa53576aeba08c0a34968ea5a4770410b89
Andrew Eames <gamerweazel@gmail.com>
2020-02-27 09:54:06 -0700

    Logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Make sure you copy the entire shared scripts folder to /usr/local/bin on your container.
 set -e
+echo "Using $ENVIRONMENT for $SERVICE service"
 eval $(aws-env $ENVIRONMENT $SERVICE)
 exec $1 "${@:2}"
